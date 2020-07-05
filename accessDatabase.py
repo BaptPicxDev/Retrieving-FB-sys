81d54e28b4f59ac0a0ba1dca476657038201211e
BaptPicxDev <picard.baptiste@laposte.net>
2020-07-05 16:07:53 +0200

    CSV and Json functions added to be able to put in ELK

--- accessDatabase.py
+++ accessDatabase.py
@@ -10,7 +10,13 @@
 #############################################
 
 # Imports
+import os
+import pandas
+import csv
+import json
 import pymongo 
+from bson import json_util
+
 
 # Functions 
 def openDatabase(json_mongoDB) : 
@@ -48,7 +54,7 @@ def eraseCollection(collection) :
 
 def fillCollection(collection, items) : 
 	"""
-		overview : fill our collection.
+		overview : fill a collection.
 			Each item is a dictionnary.
 		inputs : 
 			- collection : mongoDB collection.
@@ -60,3 +66,50 @@ def fillCollection(collection, items) :
 		collection.insert_one(item)
 		cmpt += 1
 	print('Collection filled with ',cmpt,' items.')
+
+def getCollectionItems(collection) :
+	"""
+		overview : return a collection.
+		inputs : 
+			- collection : mongoDB collection.
+			- items :  list of item (=list of dictionnary). 
+	"""
+	return list(collection.find())
+
+
+def collectionToJson(my_colletion, file_name) :
+	"""
+		overview : save a collection -> json.
+		inputs : 
+			- collection : mongoDB collection.
+	"""
+	collection = json.loads(json_util.dumps(my_colletion.find()))
+	if not os.path.exists('./data/'+file_name) :
+		with open('./data/'+file_name, 'w', encoding='utf8') as file:
+			file.write('[')
+			for index, item in enumerate(collection) : 
+				file.write(json.dumps(item, ensure_ascii=False))
+				if(index+1 != len(collection)) :
+					file.write(',')
+			file.write(']')
+		print('File ./data/'+file_name+' created.')
+
+def deleteFile(file) :
+	"""
+		overview : delete a  file.
+		inputs : 
+			- file :  file.
+	"""
+	if os.path.exists('./data/'+file) :
+		os.remove('./data/'+file)
+		print('File ',file,' deleted.')
+
+def fromJsonToCsv(json_file) : 
+	json_data = pandas.read_json('./data/'+json_file)
+	json_data = json_data.astype({'id' : str})
+	for index, row in json_data.iterrows() :
+		json_data.set_value(index, 'id', row['_id']['$oid'])
+	json_data = json_data.drop('_id', axis=1)
+	if not os.path.exists('./data/'+json_file.replace('.json', '.csv')) : 
+		json_data.to_csv('./data/'+json_file.replace('.json', '.csv'), index=False)
+		print('File ./data/',json_file.replace('.json', '.csv'),' created')
--- main.py
+++ main.py
@@ -9,6 +9,7 @@
 #############################################
 
 # Imports
+import time 
 import json # Reading .json files
 import datetime # Time computation
 from selenium import webdriver 
@@ -18,7 +19,10 @@ from selenium.webdriver.chrome.options import Options # Webdriver options
 from accessDatabase import * 
 
 # Constants
-DATA_PATH = './data/variables.json'
+EXPORT = True
+
+# Constants
+DATA_PATH = './data/variables_ex.json'
 
 if __name__ == "__main__" :
 	start = datetime.datetime.now()
@@ -26,90 +30,98 @@ if __name__ == "__main__" :
 	db = openDatabase(variables["mongoDB"])
 	db_posts = getCollection(db, variables["mongoDB"]["collections"]["posts"])
 	db_comments = getCollection(db, variables["mongoDB"]["collections"]["comments"])
-	eraseCollection(db_posts)
-	eraseCollection(db_comments)
-	options = Options() # Set up the option
-	options.binary_location = variables['chrome_path'] # Give the chrome path 
-	driver = webdriver.Chrome(chrome_options=options, executable_path=variables['chrome_webdriver_path']) # Creating the Web driver
-	driver.get("https://www.facebook.com/") # Load the facebook page
-	time.sleep(5)
-	username= driver.find_element_by_id("email") 
-	password = driver.find_element_by_id("pass") 
-	bt_log = driver.find_element_by_id("u_0_b") 
-	n_posts, n_comments = [], []
-	try : # Logging session
-		username.send_keys(variables['facebook_id'])
-		password.send_keys(variables['facebook_pwd'])
-		bt_log.click()
-		time.sleep(10)
-		driver.get("https://www.facebook.com/jchirac") # Get the fan page of Jacques Chirac
+	if(EXPORT) :
+		deleteFile('db_comment.json')
+		deleteFile('db_comment.csv')
+		collectionToJson(db_comments, 'db_comment.json') 
+		fromJsonToCsv('db_comment.json')
+	else :
+		eraseCollection(db_posts)
+		eraseCollection(db_comments)
+		options = Options() # Set up the option
+		options.binary_location = variables['chrome_path'] # Give the chrome path 
+		driver = webdriver.Chrome(chrome_options=options, executable_path=variables['chrome_webdriver_path']) # Creating the Web driver
+		driver.get("https://www.facebook.com/") # Load the facebook page
 		time.sleep(5)
-		posts = driver.find_elements_by_class_name("du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0")
-		if(len(posts)!=0) :
-			for index_post, post in enumerate(posts) :
-				print("Post number : ",index_post)
-				post_text = post.text.split('\n')
-				post_date = post_text[1]
-				post_resume = post_text[4]
-				post_n_likes = post_text[7]
-				post_n_comments = post_text[8].split(' ')[0]
-				post_n_shares = post_text[9].split(' ')[0]
-				post_href = post.find_element_by_xpath("//a[@aria-labelledby='jsc_c_2m' or 'jsc_c_1g']").get_attribute('href')
-				n_posts.append({'resume' : post_resume, 'date' : post_date, 'n_likes' : post_n_likes, 'n_comments' : post_n_comments, 'n_shares' : post_n_shares, 'link' : post_href, 'id' : index_post})
-				try : 
-					post.find_element_by_xpath("//span[contains(text(), 'View comments')]").click()
-					time.sleep(10)
-					comments = post.find_element_by_class_name("stjgntxs.ni8dbmo4.l82x9zwi.uo3d90p7.h905i5nu.monazrh9").find_elements_by_tag_name("li")
-					cmpt = 0
-					for com in comments :
-						if(len(com.text)>10) :
-							print("Post number : ",index_post," / Comment number : ", cmpt)
-							com_username = com.text.split('\n')[0]
-							com_comment = com.text.split('\n')[1]
-							if(('Likes' and 'Like') in com.text.split('\n')[-2]) :
-								if(com.text.split('\n')[-3].isdigit()) :
-									com_likes = com.text.split('\n')[-3]
+		username= driver.find_element_by_id("email") 
+		password = driver.find_element_by_id("pass") 
+		bt_log = driver.find_element_by_id("u_0_b") 
+		n_posts, n_comments = [], []
+		try : # Logging session
+			username.send_keys(variables['facebook_id'])
+			password.send_keys(variables['facebook_pwd'])
+			bt_log.click()
+			time.sleep(10)
+			driver.get("https://www.facebook.com/jchirac") # Get the fan page of Jacques Chirac
+			time.sleep(5)
+			posts = driver.find_elements_by_class_name("du4w35lb.k4urcfbm.l9j0dhe7.sjgh65i0")
+			if(len(posts)!=0) :
+				for index_post, post in enumerate(posts) :
+					print("Post number : ",index_post)
+					post_text = post.text.split('\n')
+					post_date = post_text[1]
+					post_resume = post_text[4]
+					post_n_likes = post_text[7]
+					post_n_comments = post_text[8].split(' ')[0]
+					post_n_shares = post_text[9].split(' ')[0]
+					post_href = post.find_element_by_xpath("//a[@aria-labelledby='jsc_c_2m' or 'jsc_c_1g']").get_attribute('href')
+					n_posts.append({'resume' : post_resume, 'date' : post_date, 'n_likes' : post_n_likes, 'n_comments' : post_n_comments, 'n_shares' : post_n_shares, 'link' : post_href, 'id' : index_post})
+					try : 
+						post.find_element_by_xpath("//span[contains(text(), 'View comments')]").click()
+						time.sleep(10)
+						comments = post.find_element_by_class_name("stjgntxs.ni8dbmo4.l82x9zwi.uo3d90p7.h905i5nu.monazrh9").find_elements_by_tag_name("li")
+						cmpt = 0
+						for com in comments :
+							if(len(com.text)>10) :
+								print("Post number : ",index_post," / Comment number : ", cmpt)
+								com_username = com.text.split('\n')[0]
+								com_comment = com.text.split('\n')[1]
+								if(len(com_comment)>=10000) :
+									com_comment = com_comment[:9999]
+								if(('Likes' and 'Like') in com.text.split('\n')[-2]) :
+									if(com.text.split('\n')[-3].isdigit()) :
+										com_likes = com.text.split('\n')[-3]
+									else : 
+										com_likes = '0'
+								elif(('Likes' and 'Like') in com.text.split('\n')[-3]):
+									if(com.text.split('\n')[-4].isdigit()) :
+										com_likes = com.text.split('\n')[-4]
+									else : 
+										com_likes = '0'
 								else : 
-									com_likes = '0'
-							elif(('Likes' and 'Like') in com.text.split('\n')[-3]):
-								if(com.text.split('\n')[-4].isdigit()) :
-									com_likes = com.text.split('\n')[-4]
+									com_likes = '0' 
+								if("·" in com.text.split('\n')[-1]) :
+									if(com.text.split('\n')[-1].split(" · ")[-1] != "Edited") :
+										com_date = com.text.split('\n')[-1].split(' · ')[-1]  
+									else :
+										com_date = com.text.split('\n')[-1].split(' · ')[-2] 
+								elif("·" in com.text.split('\n')[-2]) :
+									if(com.text.split('\n')[-2].split(" · ")[-1] != "Edited") :
+										com_date = com.text.split('\n')[-2].split(' · ')[-1]  
+									else :
+										com_date = com.text.split('\n')[-2].split(' · ')[-2] 
 								else : 
-									com_likes = '0'
-							else : 
-								com_likes = '0' 
-							if("·" in com.text.split('\n')[-1]) :
-								if(com.text.split('\n')[-1].split(" · ")[-1] != "Edited") :
-									com_date = com.text.split('\n')[-1].split(' · ')[-1]  
+									com_date = "No date"
+								if("·" not in com.text.split('\n')[-1]) :
+									com_n_replies = com.text.split('\n')[-1].split(' ')[0] 
 								else :
-									com_date = com.text.split('\n')[-1].split(' · ')[-2] 
-							elif("·" in com.text.split('\n')[-2]) :
-								if(com.text.split('\n')[-2].split(" · ")[-1] != "Edited") :
-									com_date = com.text.split('\n')[-2].split(' · ')[-1]  
-								else :
-									com_date = com.text.split('\n')[-2].split(' · ')[-2] 
-							else : 
-								com_date = "No date"
-							if("·" not in com.text.split('\n')[-1]) :
-								com_n_replies = com.text.split('\n')[-1].split(' ')[0] 
+									com_n_replies = '0'
+								cmpt = cmpt + 1
+								comment = {'user' : com_username, 'comment' : com_comment,  'likes' : com_likes, 'date' : com_date, 'n_replies' : com_n_replies, 'id_post' : index_post, 'id' : cmpt}
+								n_comments.append(comment)
 							else :
-								com_n_replies = '0'
-							cmpt = cmpt + 1
-							comment = {'user' : com_username, 'comment' : com_comment,  'likes' : com_likes, 'date' : com_date, 'n_replies' : com_n_replies, 'id_post' : index_post, 'id' : cmpt}
-							n_comments.append(comment)
-						else :
-							pass
-					fillCollection(db_posts, n_posts)
-					fillCollection(db_comments, n_comments)
-					break
-				except Exception as exc_btn :
-					print("Can't load the data")
-					print(exc_btn)
-		else : 
-			print('No posts')
-		driver.close()
-		print(n_posts)
-	except Exception as exc :
-		print("Error while logging")
-		print(exc)
+								pass
+						fillCollection(db_posts, n_posts)
+						fillCollection(db_comments, n_comments)
+						break
+					except Exception as exc_btn :
+						print("Can't load the data")
+						print(exc_btn)
+			else : 
+				print('No posts')
+			driver.close()
+			print(n_posts)
+		except Exception as exc :
+			print("Error while logging")
+			print(exc)
 	print("It takes {} seconds to reach the end of the script.".format(datetime.datetime.now()-start))
